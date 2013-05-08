77d8a4940fcb96ee35e3d5e08a99f08e5f2fb50b
briangilbreath <bgilbreath87@gmail.com>
2013-05-08 17:31:59 -0400

    weather search through openweather map works, basic

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,40 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
+/*angular.module('myApp.controllers', []).
   controller('MyCtrl1', [function() {
 
   }])
   .controller('MyCtrl2', [function() {
 
-  }]);
+  }]);*/
+
+function WeatherCtrl($scope, $http) {
+
+
+ 
+	$scope.update = function(weather) {
+	    var master = angular.copy(weather);
+
+	    $http.jsonp('http://api.openweathermap.org/data/2.5/weather?q=' + master.location + '&callback=JSON_CALLBACK').success(function(json) {
+        console.log(json);
+      	$scope.weather = json;
+ 	});
+
+	};
+	 
+	$scope.reset = function() {
+	    $scope.weather = angular.copy($scope.master);
+	};
+	 
+	$scope.reset();
+  
+
+
+
+
+
+  $scope.hello = "Hello, World!"
+
+}
+
